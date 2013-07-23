8b4688a7111604c293b1b98ab466913a9791cb26
RocketGun <jonas@mischlicht.com>
2013-07-23 10:37:14 +0200

    Update saveRefFrames.py

--- nuke/safeRefFrames/saveRefFrames.py
+++ nuke/safeRefFrames/saveRefFrames.py
@@ -1,54 +1,60 @@
-
-import nuke
 import os
+import nuke
 
 
 def saveFrame(filePath = ""):
-	viewer = nuke.activeViewer()
-	inputNode = nuke.ViewerWindow.activeInput(viewer)
-	viewNode = nuke.activeViewer().node()
+    viewer = nuke.activeViewer()
+    inputNode = nuke.ViewerWindow.activeInput(viewer)
+    viewNode = nuke.activeViewer().node()
 
-	if inputNode != None:
-		selInput = nuke.Node.input(viewNode, inputNode)
+    if inputNode != None:
+        selInput = nuke.Node.input(viewNode, inputNode)
 
-		if filePath = "":
-			filePath = nuke.getFilename('Save File', "*.png *.jpg *.tga *.exr *.dpx", type = 'save')
-		
-
-
-		if filePath != None:
-
-			#Check Path
-			path, ext = os.path.split(filePath)
-			if not os.path.isdir(path):
-				pass
-				### TODO: Build Path			
+        if filePath = "":
+            filePath = nuke.getFilename('Save File', "*.png *.jpg *.tga *.exr *.dpx", type = 'save')
+        
 
 
-			write = nuke.nodes.Write(file = filePath, name = 'WriteSaveThisFrame', file_type=ext)
-			write.setInput(0,selInput)
-			curFrame = int(nuke.knob("frame"))
-			nuke.execute(write.name(), curFrame, curFrame)
-			nuke.delete(write)
+        if filePath != None:
 
-	else:
-		nuke.message("This viewer don't have any input connected!")
+            #Check Path
+            path, ext = os.path.split(filePath)
+            if not os.path.isdir(path):
+                pass
+                ### TODO: Build Path            
 
-def get
+
+            write = nuke.nodes.Write(file = filePath, name = 'WriteSaveThisFrame', file_type=ext)
+            write.setInput(0,selInput)
+            curFrame = int(nuke.knob("frame"))
+            nuke.execute(write.name(), curFrame, curFrame)
+            nuke.delete(write)
+
+    else:
+        nuke.message("This viewer don't have any input connected!")
+
+def getSeqName():
+    res = ""    
+    scriptpath = str.split(nuke.root().knob('name').value() , "/")    
+    res = scriptpath[len(scriptpath)-3]
+    return res
+
+
+def getNextRefFrame():
+    ret = "rf01"
 
 
 def getRefFramePath():
-	res = ""
+    res = ""
 
-	res = " /Users/jonas/Desktop/test1.ext"
+    res = " /Users/jonas/Desktop/test1.ext"
 
-	return res
+    return res
 
-def getSeqName():
-	return "0009"
+
 
 def saveRefFrame():
-	refPath = getRefFramePath()
-	seqName = getSeqName()
+    refPath = getRefFramePath()
+    seqName = getSeqName()
 
-	saveFrame(os.path.join(getRefFramePath(), getSeqName(), getSeqName() + ".exr" ))
+    saveFrame(os.path.join(getRefFramePath(), getSeqName(), getSeqName() + "_" + getNextRefFrame() + ".exr" ))
